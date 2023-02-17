3a3b3f53b323e2c1b66e617026723372b898689d
Sergey Lukashevich <rs.lukashevich@gmail.com>
2023-02-17 19:26:59 +0300

    init: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = String(Math.abs(n));
+  //let str = String(n).replace('-','');
+  let a = '';
+
+  for (let i = str.length-1; i > -1; i -= 1) 
+    {a += str[i]; }
+
+  return a
 }
